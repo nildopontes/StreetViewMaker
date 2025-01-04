bc5b2aa5e45316a1cedf3baa519d7b9761cea776
Nildo Vieira Pontes <nildo.pontes@bol.com.br>
2025-01-03 23:10:02 -0300

    Add files via upload

--- photosphere.js
+++ photosphere.js
@@ -1,5 +1,6 @@
 const CLIENT_ID = '824279225081-mmrvha4gr4l13jp7d9k25a917mu2elg0.apps.googleusercontent.com';
 const SCOPE = 'https://www.googleapis.com/auth/streetviewpublish';
+const API_KEY = 'AIzaSyDjlX0EgBOzuIs9y-ibufnqnYLkJpPAhxU';
 var client = null;
 var access_token = null;
 
@@ -10,8 +11,8 @@ function initClient(){
       prompt: '',
       callback: tokenResponse => {
          access_token = tokenResponse.access_token;
-         sessionStorage.setItem('secToken', access_token);
-         sessionStorage.setItem('expToken', parseInt(Date.now()/1000, 10) + tokenResponse.expires_in - 60); // 60 segundos de margem de segurança
+         sessionStorage.setItem('secureToken', access_token);
+         sessionStorage.setItem('expireToken', parseInt(Date.now()/1000, 10) + tokenResponse.expires_in - 60); // 60 segundos de margem de segurança
          console.log(access_token);
       },
    });
@@ -24,11 +25,39 @@ Gera um novo token de acesso OAuth ou retorna o atual caso ainda esteja válido
 */
 function newToken(){
    return  new Promise((resolve, reject) => {
-      if(sessionStorage.getItem('secToken') !== null && parseInt(Date.now()/1000, 10) < parseInt(sessionStorage.getItem('expToken'), 10)){
-         resolve(sessionStorage.getItem('secToken'));
+      if(sessionStorage.getItem('secureToken') !== null && parseInt(Date.now()/1000, 10) < parseInt(sessionStorage.getItem('expireToken'), 10)){
+         resolve(sessionStorage.getItem('secureToken'));
       }else{
          client.requestAccessToken();
          resolve(access_token);
       }
    });
+}
+
+/*
+Solicita uma URL para upload de foto.
+@param {String} token - o token de acesso OAuth 2.0
+*/
+function getUploadURL(token){
+   const url = `https://streetviewpublish.googleapis.com/v1/photo:startUpload?key=${API_KEY}`;
+   return new Promise((resolve, reject) => {
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', url, true);
+      xhr.setRequestHeader('Content-Length', '0');
+      xhr.setRequestHeader('Authorization', `Bearer ${token}`);
+      xhr.onreadystatechange = function() {
+         if(xhr.readyState == 4){
+            if(xhr.status >= 200 && xhr.status <= 206){
+               console.log(xhr.reponseText);
+            }else{
+               reject(`Error: status code ${xhr.status}.`);
+            }
+         }
+      }
+      xhr.send(data);
+   });
+}
+
+function sendImageData(){
+   
 }
